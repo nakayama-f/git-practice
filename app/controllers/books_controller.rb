7af9ab7354cae8389802bf6ab811ac8d205abf69
FusaNkayama <99817687+nakayama-f@users.noreply.github.com>
2022-03-21 16:29:43 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all.order(created_at: :desc)
     @book_new = Book.new
+
   end
   def show
     @book = Book.find(params[:id])
